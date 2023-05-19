858f9ca05acea50ef0ab6a3a070fff2a9c91864d
jjortiz016 <jjortiz016@misena.edu.co>
2023-05-19 12:27:41 -0500

    Archivos iniciales del proyecto

---
+++ src/main/java/com/examenes/examenesbackend/ExamenesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.examenes.examenesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examenes/examenesbackend/ExamenesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examenes.examenesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
