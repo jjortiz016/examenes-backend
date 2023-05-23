e2d2169230af3cd563b816537e7c891ff33f3deb
jjortiz016 <jjortiz016@misena.edu.co>
2023-05-22 20:22:34 -0500

    se crearon manualmente los objetos en la clase ExamenesBackendApplication y se realizo prueva de inserción a la base de datos del usuario y los roles

--- src/main/java/com/examenes/examenesbackend/ExamenesBackendApplication.java
+++ src/main/java/com/examenes/examenesbackend/ExamenesBackendApplication.java
@@ -1,14 +1,48 @@
 package com.examenes.examenesbackend;
 
+import com.examenes.examenesbackend.entities.Role;
+import com.examenes.examenesbackend.entities.User;
+import com.examenes.examenesbackend.entities.UserRole;
+import com.examenes.examenesbackend.services.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 @SpringBootApplication
-public class ExamenesBackendApplication {
-
+public class ExamenesBackendApplication implements CommandLineRunner {
+    @Autowired
+	private IUserService userService;
 	public static void main(String[] args) {
 		SpringApplication.run(ExamenesBackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		User user= new User();
+
+		user.setName("Jhon");
+		user.setLastName("Ortiz");
+		user.setUsername("jortiz");
+		user.setPassword("1234");
+		user.setEmail("correo@correo.com");
+		user.setPhone("2121546131");
+		user.setProfile("photo.png");
+
+		Role role = new Role();
+		role.setIdRole(1L);
+		role.setName("ADMIN");
+
+		Set<UserRole> userRoles = new HashSet<>();
+		UserRole userrole= new UserRole();
+		userrole.setRole(role);
+		userrole.setUser(user);
+		userRoles.add(userrole);
+
+		User userSaved = userService.saveUser(user, userRoles );
+	}
 }
